181457164f4d61fb0cd0a4baee9c99f7a9903b61
huangcy <huangcy@csdn.net>
2015-04-09 10:25:21 +0800

    change src

--- src/main/java/com/blogchong/storm/helloworld/spout/RandomSentenceSpout.java
+++ src/main/java/com/blogchong/storm/helloworld/spout/RandomSentenceSpout.java
@@ -42,17 +42,20 @@ public class RandomSentenceSpout extends BaseRichSpout {
 				"jikexueyuan is a good school",
 				"And if the golden sun",
 				"four score and seven years ago",
+				"storm hadoop spark hbase",
 				"blogchong is a good man",
 				"Would make my whole world bright",
 				"blogchong is a good website",
-				"It would have to be with you",
+				"storm would have to be with you",
+				"Pipe to subprocess seems to be broken No output read",
 				" You make me feel so happy",
 				"For the moon never beams without bringing me dreams Of the beautiful Annalbel Lee",
-				"I love jikexueyuan and blogchong",
+				"Who love jikexueyuan and blogchong",
 				"blogchong.com is Magic sites",
-				"I swayed my leaves and flowers in the sun",
+				"Ko blogchong swayed my leaves and flowers in the sun",
 				"You love blogchong.com", "Now I may wither into the truth",
 				"That the wind came out of the cloud",
+				"at backtype storm utils ShellProcess",
 				"Of those who were older than we" };
 		// 从sentences数组中，随机获取一条语句，作为这次spout发送的消息
 		String sentence = sentences[random.nextInt(sentences.length)];
